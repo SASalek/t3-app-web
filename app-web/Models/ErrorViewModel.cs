0e42c2877d94474851002f7fd8afcf7630e5d751
SASalek <solange.salek@davinci.edu.ar>
2022-06-13 00:50:42 -0300

    Agregar archivos de proyecto.

---
+++ app-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace app_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
